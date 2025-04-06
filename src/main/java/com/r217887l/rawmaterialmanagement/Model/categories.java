7c29df4b9000f32b63cf6e55b51054315e4a36af
blessing chibvongodze <elvischibvox@gmail.com>
2025-04-06 18:12:56 +0200

    Add to remote GitHub

--- src/main/java/com/r217887l/rawmaterialmanagement/Model/categories.java
+++ src/main/java/com/r217887l/rawmaterialmanagement/Model/categories.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.r217887l.rawmaterialmanagement.Model;
 
-/**
- *
- * @author Elvis
- */
 public class categories {
+    
+    private String name;
+
+    public categories() {
+    }
+
+    public categories(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     
 }
