3d899bd146157ab43461f1d5ec7f5894c0477484
Wendelaar <tinavoishe01@gmail.com>
2025-04-06 17:15:02 +0200

    Add to remote GitHub

--- src/main/java/com/r217887l/rawmaterialmanagement/Repo/UsersRepo.java
+++ src/main/java/com/r217887l/rawmaterialmanagement/Repo/UsersRepo.java
@@ -1,7 +1,36 @@
 
 package com.r217887l.rawmaterialmanagement.Repo;
 
+import DAO.DAOCONNECT;
+import com.r217887l.rawmaterialmanagement.Model.Users;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class UsersRepo {
+    
+    public void saveUser(Users users){
+    
+        Connection dbconn = DAOCONNECT.connectdb();
+        
+        try {
+            PreparedStatement st = (PreparedStatement)
+                    dbconn.prepareStatement("INSERT INTO Users "
+                            + "(firstName,lastName,userName,roles,pass) "
+                            + "VALUES(?,?,?,?,?)");
+            st.setString(1, users.getFirstName());
+            st.setString(2, users.getLastName());
+            st.setString(3, users.getUserName());
+            st.setString(4, users.getRoles());
+            st.setString(5, users.getPassword());
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersRepo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 
 }
